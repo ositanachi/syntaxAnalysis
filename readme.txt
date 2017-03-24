163794dbd12425437d6ccd219facb7ee352249db
Osi Otugo <ositanachi.otugo@bison.howard.edu>
2017-03-23 23:36:24 -0400

    Readme

---
+++ readme.txt
@@ -0,0 +1,9 @@
+https://github.com/ositanachi/syntaxAnalysis
+
+https://github.com/ositanachi/syntaxAnalysis/commit/ce1f8691fbf5ea491895cb9b8aa858314162752a
+Initial commit
+
+https://github.com/ositanachi/syntaxAnalysis/commit/ce1f8691fbf5ea491895cb9b8aa858314162752a
+I could not do a git commit for each commit since my computer was messed up but I managed to recover it from email
+
+No special instructions
--- syntax.c
+++ syntax.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-#include <ctype.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 int indexLine;
-//
-int lexLen;
 int charClass;
-//
+int lexLen;
 int token;
 int nextToken;
 size_t expression_length = 0;
@@ -17,7 +15,7 @@ char endCharacter;
 char * expression = NULL;
 FILE *in_fp, *fopen();
 
-//Functions
+//*** Function Declarations ***//
 void addChar();
 void getChar();
 void getNonBlank();
@@ -29,13 +27,13 @@ void term();
 void factor();
 void error();
 
-//Characters
+//*** Character Classes ***//
 #define LETTER 0
 #define DIGIT 1
 #define NEWLINE 2
 #define UNKNOWN 99
 
-//Token Codes
+//*** Token Codes ***//
 #define INT_LIT 10
 #define IDENT 11
 #define ASSIGN_OP 20
@@ -46,13 +44,13 @@ void error();
 #define LEFT_PAREN 25
 #define RIGHT_PAREN 26
 
-main(int argco, char* argv[]) { 
-  if (argc != 2) {
-      printf("ERROR! NO FILE GIVEN!\n");
+main(int argo, char* argv[]) { 
+  if (argo != 2) {
+      printf("NO FILE GIVEN!\n");
       exit(0);
   }
   if ((in_fp = fopen(argv[1], "r")) == NULL)
-    printf("ERROR - Cannot open front.in \n");
+    printf("ERROR - cannot open front.in \n");
   else {
     while ((read_exression = getline(&expression, &expression_length, in_fp)) != EOF) {
       indexLine = 0;
@@ -65,4 +63,152 @@ main(int argco, char* argv[]) {
       }
     }
   }
+}
+
+int lookup(char ch) {
+  switch (ch) {
+    case '(':
+      addChar();
+      nextToken = LEFT_PAREN;
+      break;
+    case ')':
+      addChar();
+      nextToken = RIGHT_PAREN;
+      break;
+    case '+':
+      addChar();
+      nextToken = ADD_OP;
+      break;
+    case '-':
+      addChar();
+      nextToken = SUB_OP;
+      break;
+    case '*':
+      addChar();
+      nextToken = MULT_OP;
+      break;
+    case '/':
+      addChar();
+      nextToken = DIV_OP;
+      break;
+    default:
+      addChar();
+      nextToken = EOF;
+      break;
+    }
+    return nextToken;
+}
+
+void addChar() {
+  if (lexLen <= 98) {
+    lexeme[lexLen++] = nextChar;
+    lexeme[lexLen] = 0;
+  }
+  else
+    printf("Error - lexeme is too long \n");
+}
+
+void getChar() {
+  if (expression[indexLine] != '\n' && expression[indexLine] != '\0') {
+      nextChar = expression[indexLine++];
+    if (isalpha(nextChar)) {
+      charClass = LETTER;
+    }
+    else if (isdigit(nextChar)) {
+      charClass = DIGIT;
+    }
+    else if (nextChar == '\n')
+      charClass = NEWLINE;
+    else charClass = UNKNOWN;
+  }
+  else
+  charClass = EOF;
+}
+
+void getNonBlank() {
+  while (isspace(nextChar))
+    getChar();
+}
+
+int lex() {
+  lexLen = 0;
+  getNonBlank();
+  endCharacter = nextChar;
+  switch (charClass) {
+    case LETTER:
+      addChar();
+      getChar();
+      while (charClass == LETTER || charClass == DIGIT) {
+        addChar();
+        getChar();
+      }
+      nextToken = IDENT;
+      break;
+    case DIGIT:
+      addChar();
+      getChar();
+      while (charClass == DIGIT) {
+      addChar();
+      getChar();
+      }
+      nextToken = INT_LIT;
+      break;
+    case UNKNOWN:
+      lookup(nextChar);
+      getChar();
+      break;
+    case EOF:
+      nextToken = EOF;
+      lexeme[0] = 'E';
+      lexeme[1] = 'O';
+      lexeme[2] = 'F';
+      lexeme[3] = 0;
+      break;
+  }
+    printf("Next token is: %d, Next lexeme is %s\n",
+    nextToken, lexeme);
+    return nextToken;
+}
+
+void expr() {
+  printf("Enter <expr>\n");
+  term();
+  while (nextToken == ADD_OP || nextToken == SUB_OP) {
+    lex();
+    term();
+  }
+  printf("Exit <expr>\n");
+}
+
+void term() {
+  printf("Enter <term>\n");
+  factor();
+  while (nextToken == MULT_OP || nextToken == DIV_OP) {
+    lex();
+    factor();
+  }
+  printf("Exit <term>\n");
+}
+
+void factor() {
+  printf("Enter <factor>\n");
+  if (nextToken == IDENT || nextToken == INT_LIT)
+    lex();
+  else {
+    if (nextToken == LEFT_PAREN) {
+      lex();
+      expr();
+      if (nextToken == RIGHT_PAREN)
+        lex();
+      else
+        error();
+    } 
+    else
+      error();
+  }
+  printf("Exit <factor>\n");;
+}
+
+void error() {
+  printf("!! Error has occurred and been caught at %c !! The expression is %s ****\n", endCharacter, expression);
 }
